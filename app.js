4febaafde39277315fa2d6e46d54a3aa6004f8ca
ayelabilal <ayelabilal944@gmail.com>
2024-02-23 18:05:46 +0500

    assignment continue

---
+++ app.js
@@ -0,0 +1,116 @@
+// chapter  no 1 "VARIABLE NAME LEGAL AND ILLEGAL"
+
+// q:1
+var  firstName ="Hello world"
+var lastName = "Hello students"
+var number = "25"
+
+// q:2 (5 legal variable)
+var $20 = "Hello!"
+var rollNo = "5432"
+var name = "ayela"
+var $email = "@gmail.com"
+var xy_23 = "xyz"
+
+// (illega variable)
+// var 123 ="numbers"
+// var @name = "names"
+// var %20 = "pecentage"
+// var # = "hashtag"
+// var * = "multiple"
+
+// q:3
+// a
+// var name = "rules"
+document.write ("<h3> a:Rules for naming JS variables</h3>");
+// b
+document.write("<br>b: variable names can only contain numbers, $, text and camel case format.")
+// c
+document.write("<br>c: variable must begin with a letter,$,or Camel case format.")
+// d
+document.write("<br>d: variables names are case sensitive.")
+// e
+document.write("<br>e: variable names should no be JS keywords.")
+
+// chapter no 5  MATH EXPRESSIONS
+// 1
+document.write("<br><h4>MATH EXPRESSIONS</h4>")
+var number = 3
+var number2 = 5
+document.write(number + number2)
+document.write( "<br>1) sum of 3 and 5 is 8")
+
+// 2
+var number3 = 3
+var number4 = 5
+document.write( "<br>","<br>",  number3 - number4)
+document.write( "<br> 2) subtracting 3 from 5 result in -2")
+
+var number5 = 3
+var number6 = 5
+document.write( "<br>", "<br>", number5 * number6)
+document.write( "<br> multiply 3 and 5 answer will be 15")
+
+// 3
+// a
+
+// b
+
+// 4
+var movieTicket= "600"
+var buy5 = "5"
+document.write("<br><br>", movieTicket * buy5)
+document.write( "<br> 4) Total cost to buy 5 tickets to a movie is 3000PKR")
+
+// 5
+document.write("<br>,<h3> 5) Table of 4 </h3>")
+var table = "4"
+document.write("<br>", "4 * 1 =" +table + 
+"<br>", "4 * 2 =" +  2* table+
+"<br>", "4 * 3 =" + 3* table +
+"<br>", "4 * 4 =" + 4* table+
+"<br>", "4 * 5 =" + 5* table+
+"<br>", "4 * 6 =" + 6* table+
+"<br>", "4 * 7 =" +7* table+
+"<br>", "4 * 8 =" + 8* table+
+"<br>", "4 * 9 =" + 9* table+
+"<br>", "4 * 10 =" + 10* table)
+
+// 6
+// // a
+// var celsiusTemperature =  "25"
+//  document.write(25,"<sup>0</sup>","C is 77")
+
+// 7
+document.write("<br><h4>Shopping Cart</h4>")
+var priceItem1 =  650
+var orderQty1 =   3
+document.write("<br>", "item 1 ",  priceItem1 * orderQty1 )
+
+
+var priceItem2 = 100
+var orderQty2 =   7
+var shippingCharges = 100
+document.write("<br> item 2 ",  priceItem2 * orderQty2 + shippingCharges)
+ document.write(" <br> Total 2750")
+
+//  8 
+document.write("<br> <h4>Marks Sheet</h4>")
+document.write("Total marks:90 <br> Marks obtained:804 <br>percentage:82.04%")
+
+// 9
+// document.write("<br> <h4>Currency in PKR</h4> ")
+
+// 11
+document.write("<br>","<h4>The Age Calculator</h4>")
+var currentYear= 2016
+var birthYear = 1992
+document.write("<br>", "Current year: 2016" ,"<br>", "Birth Year:1992","<br>","Your Age: 24")
+
+// 12 
+document.write("<br>","<h4>The Geometrizer</h4>")
+var radius= 20
+document.write("<br>","Radius of a circle: 20","<br> The circumference is:125.67999","<br> The area is:1256.8")
+
+// chapter no 6 MATH EXPRESSIONS
+document.write("<br>","<h3>MATH EXPRESSIONS</h3>")
